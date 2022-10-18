a7f47dcef9e1d02b8810268dc007749ab24c5293
andrey.blednykh <blednykhap@gmail.com>
2022-10-18 07:47:54 +0300

    Work with hive

--- README.md
+++ README.md
@@ -33,6 +33,13 @@
       .master("yarn")
       .config("spark.hadoop.fs.defaultFS", "hdfs://192.168.231.144:8020")
       .config("spark.hadoop.yarn.resourcemanager.address", "192.168.231.144:8050") // :8032
-      .config("spark.yarn.jars", "hdfs://192.168.231.144:8020/user/Sinoptik/jars/*.jar")
+      .config("spark.yarn.jars", "hdfs://192.168.231.144:8020/user/tonny/jars/*.jar")
+      .config("hive.metastore.uris", "thrift://192.168.231.146:9083")
+      .enableHiveSupport()
       .getOrCreate()
+    ```
+8. Add admin rights to the user
+
+    ```
+        sudo usermod -a -G wheel tonny
     ```
--- src/main/scala/org/sinoptik/test_debug.scala
+++ src/main/scala/org/sinoptik/test_debug.scala
@@ -16,7 +16,9 @@ object test_debug {
       .config("spark.yarn.jars", "hdfs://192.168.231.144:8020/user/Sinoptik/jars/*.jar")
       //.config("spark.hadoop.yarn.application.classpath", "$HADOOP_COMMON_HOME/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HIVE_HOME/*,$HIVE_HOME/*,$HIVE_HOME/lib/*")
       //.config("yarn.application.classpath", "$HADOOP_CONF_DIR,/usr/lib/hadoop/*,/usr/lib/hadoop/lib/*,/usr/lib/hadoop-hdfs/*,/usr/lib/hadoop-hdfs/lib/*,/usr/lib/hadoop-yarn/*,/usr/lib/hadoop-yarn/lib/*,/usr/lib/hadoop-mapreduce/*,/usr/lib/hadoop-mapreduce/lib/*")
-      //.enableHiveSupport()
+      .config("hive.metastore.uris", "thrift://192.168.231.146:9083")
+      //.config("hive.metastore.warehouse.dir", "hdfs://192.168.231.146:8020/apps/hive/warehouse")
+      .enableHiveSupport()
       .getOrCreate()
 
     println("========== Spark Remote Debug (finish) ==========")
